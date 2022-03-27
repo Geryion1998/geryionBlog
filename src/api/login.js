d0e36653a75332c0fb0228b6e4e31b411a21afc7
yqliu <yqliu@linx-infoc.com>
2022-03-27 17:05:43 +0800

    init vue code.

---
+++ src/api/login.js
@@ -0,0 +1,27 @@
+import request from '@/utils/request'
+
+export function login(username, password) {
+  return request({
+    url: 'http://localhost:8080/user/login',
+    method: 'post',
+    data: {
+      username,
+      password
+    }
+  })
+}
+
+export function getInfo(token) {
+  return request({
+    url: 'http://localhost:8080/user/info',
+    method: 'get',
+    params: { token }
+  })
+}
+
+export function logout() {
+  return request({
+    url: 'http://localhost:8080/user/logout',
+    method: 'post'
+  })
+}
